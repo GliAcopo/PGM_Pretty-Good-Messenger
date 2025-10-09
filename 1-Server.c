5f878688537cc699728c954312bb8542e56355e9
Gli_acopo <jacoporizzuto04@gmail.com>
2025-10-09 23:26:30 +0200

    Created a whole ass print_local_ip_address function over server.c

--- 1-Server.c
+++ 1-Server.c
@@ -9,13 +9,20 @@
  *
  */
 
+#define _GNU_SOURCE     // To get defns of NI_MAXSERV and NI_MAXHOST 
+                        // WARNING: apparently this must be defined before any other inclusion, (I learned the HARD way...)
 #include "3-Global-Variables-and-Functions.h"
 #include "1-Server.h"
 #include <stdio.h>      // printf, fprintf
 #include <stdlib.h>     // exit, EXIT_FAILURE, EXIT_SUCCESS
+// #include <unistd.h>     // close
 #include <sys/socket.h> // socket, bind, listen, accept
 #include <netinet/in.h> // struct sockaddr_in, INADDR_ANY
 #include <arpa/inet.h>  // inet_ntoa
+#include <ifaddrs.h>    // struct ifaddrs, getifaddrs, freeifaddrs
+#include <netdb.h>      // NI_MAXHOST, getnameinfo
+// #include <linux/if_link.h> // IFLA_ADDRESS
+
 
 // SIMPLE PRINT STATEMENT ON STDOUT
 #define P(fmt, ...) do{fprintf(stdout,"[SRV]>>> " fmt "\n", ##__VA_ARGS__);}while(0);
@@ -25,6 +32,53 @@
 
 // ierror, an internal debug substitute to errno
 ERROR_CODE ierrno = NO_ERROR;
+
+
+/**
+ * @brief Prints all the local IP addresses of the machine
+ * @param port The port number to print alongside the IP addresses
+ * @return ERROR_CODE NO_ERROR on success, SYSCALL_ERROR on failure
+ * @note This function is inspired to the linux man example getifaddrs(3)
+ */
+ERROR_CODE print_local_ip_addresses(uint16_t port)
+{
+    // We use getifaddrs to get the list of network interfaces on the machine
+    struct ifaddrs *ifaddr;   // why bother allocating two pointers? because ifsddrs is actually a linked list! So we need to maintain a head pointer or else we won't be able to free the memory
+    if (getifaddrs(&ifaddr) == -1) // WARNING: allocates the ifaddrs structure by itself, so there is a need to free it later NOT WITH FREE() (man 3 getifaddrs)
+    {
+        PSE("getifaddrs() error");
+        return SYSCALL_ERROR;
+    }
+
+    P("Local IP addresses found:");
+    // Walk through linked list, maintaining head pointer so we can free list later
+    for (struct ifaddrs *ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        if (ifa->ifa_addr == NULL){
+            continue;
+        }
+
+        int family = ifa->ifa_addr->sa_family;
+
+        // For an AF_INET* interface address, display the address
+        if (family == AF_INET) // AF_INET = IPv4 ; AF_INET6 = IPv6 (We do not use IPv6...)
+        {
+            char host[NI_MAXHOST];
+            int s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if (s != 0)
+            {
+                PSE("getnameinfo() failed: %s", gai_strerror(s)); // gai_strerror (copypasted from man page) converts the error code returned by getnameinfo to a human readable string
+                freeifaddrs(ifaddr); // Edge case in wich we free (forgot it twice...)
+                return SYSCALL_ERROR;
+            }
+            P("\tInterface: %s\tAddress: %s:%u", ifa->ifa_name, host, port);
+        }
+    }
+
+    freeifaddrs(ifaddr); // Ah, free at last
+    return NO_ERROR;
+}
+
 
 /* █████████████████████████████████████████████████████████████████████████████████████████████████████████████ */
 /*                                                   MAIN LOOP                                                   */
@@ -49,9 +103,9 @@ int main(int argc, char** argv)
     }
     P("Socket created successfully, socket file descriptor: %d", skt_fd);
     
-    struct sockaddr_in address;
+    struct sockaddr_in address = {0}; // Initializing the strct to 0
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP address
+    address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP address (“accept connections on whatever IPs this host currently has.”)
     address.sin_port = 0;                 // Ephimeral port since we do not care, let the OS choose the first free one, actually a good thing since we do not want to worry about clashing port numbers
     if (unlikely(bind(skt_fd, (struct sockaddr *)&address, sizeof(address)) < 0))
     {
@@ -73,16 +127,22 @@ int main(int argc, char** argv)
         // inet_ntop() binary -> text
         // inet_pton() text -> binary
     // ntohs converts the port number from network byte order (big endian) to host byte order (little endian or whatever)
+    
+    uint16_t port = ntohs(address.sin_port);
     do{ // We use a do while loop to keep the stack clean
-        char temp_addr_str[] = "ddd.ddd.ddd.ddd"; // (String copied from linux man) Max length of an IPv4 address in string format is 15 characters + null terminator
-                                            // If this string is printed we now there was an error somewhere
+        char temp_addr_str[] = "ddd.ddd.ddd.ddd";                                                    // (String copied from linux man) Max length of an IPv4 address in string format is 15 characters + null terminator
+                                                                                                     // If this string is printed we now there was an error somewhere
         if (unlikely(inet_ntop(AF_INET, &address.sin_addr, temp_addr_str, sizeof(address)) == NULL)) // AF_INET = IPV4 ; "src  points  to  a  struct  in_addr" ;
         {
             PSE("inet_ntop returned an error");
             E();
         }
-        P("Server listening on IP: %s, Port: %d", temp_addr_str, ntohs(address.sin_port));
+        P("Server listening on IP: %s, Port: %u", temp_addr_str, port);
     }while (0);
+
+    P("Now discovering machine IP addresses...");
+    print_local_ip_addresses(port);
+    P("You can copypaste any of these IP addresses on the client machine to connect to the server");
 
     if (unlikely(listen(skt_fd, MAX_BACKLOG) < 0)) // The second parameter is the backlog, the number of connections that can be waiting while the process is handling a particular connection, 3 is a good value for now
     {
